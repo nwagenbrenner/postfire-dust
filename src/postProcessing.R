f400b01dadb1d4b1fad50ad91f5d872be926ced0
nwagenbrenner <nwagenbrenner@gmail.com>
2014-10-24 09:24:55 -0600

    add post-processing stuff

--- src/postProcessing.R
+++ src/postProcessing.R
@@ -32,3 +32,20 @@ fire.ll <- spTransform(fire, CRS("+proj=longlat +ellps=WGS84"))
 kml<-kmlPolygon(obj=fire.ll, kmlfile="perimeter.kml",
 name="R Polygon", description="", col=NULL, visibility=1, lwd=1, border=1,
 kmlname="baboon_perimeter", kmldescription="")
+
+#--------------------------------------------------------
+#  Create raster bricks
+#--------------------------------------------------------
+d<-raster('/home/natalie/postfire_emissions/BABOON_dust.tif', 480)
+d.hourly<-d*res(d)[1]^2*60*60 #mg/m2/s to total (for hour) mg per pixel
+d.total<-sum(values(d.hourly), na.rm=TRUE) #total mg for band
+
+dust<-brick('/home/natalie/postfire_emissions/BABOON_dust.tif')
+dust.sum<-sum(dust) #add up all values per pixel
+dust.sum.hourly<-dust.sum*res(dust.sum)[1]^2*60*60 #mg/m2/s to total (for hour) mg per pixel
+dust.total<-sum(values(dust.sum.hourly), na.rm=TRUE) #total mg for band
+dust.total<-dust.total/1000/1000 # mg to kg (total within burn perimeter)
+
+
+
+
