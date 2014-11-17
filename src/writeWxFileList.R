080c2c7e1882dda89145a976647289c75f9b0b32
nwagenbrenner <nwagenbrenner@gmail.com>
2014-11-17 08:48:40 -0700

    account for rain and snow

--- src/writeWxFileList.R
+++ src/writeWxFileList.R
@@ -10,6 +10,18 @@ load("/media/Elements/postfire_emissions/wx_lists/wxFileLists.RData") #load wx f
 dsn<-'/media/Elements/postfire_emissions/fires'
 ogrList<-ogrListLayers(dsn)
 
+
+#--------------------------------------------------------
+#  account for rain/snow
+#--------------------------------------------------------
+gdallocationinfo<-'/home/natalie/src/gdal_1.9.1/build/bin/./gdallocationinfo -valonly -b'
+band <-  '34 -wgs84'
+nam_file <- '/vsitar//media/Elements/postfire_emissions/NAM/0812/nam_218_2012080918.g2.tar/nam_218_20120809_1800_006.grb2'
+lon <- '-113.33'
+lat <- '45.81'
+
+system(paste(gdallocationinfo, band, nam_file, lon, lat), intern=FALSE, wait=TRUE)
+
 #--------------------------------------------------------
 #  write nam_i.csv files
 #--------------------------------------------------------
@@ -27,7 +39,11 @@ for(i in 1:120){
 
 #full year
 for(i in 1:length(ogrList)){
+    if(i==82){
+        next
+    } 
     wx<-get(paste0("wxFileList_", i))
     path<-'/media/Elements/postfire_emissions/wx_lists/'
     write.table(wx, paste0(path, 'nam_', i, '.csv'), row.names=FALSE, col.names=FALSE, quote=FALSE)
 }
+
