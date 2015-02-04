17510b46f90a39ff4f1e1f7b2966d43d77cca2d2
nwagenbrenner <nwagenbrenner@gmail.com>
2015-02-04 15:48:55 -0700

    process annual files

--- src/postProcessing.R
+++ src/postProcessing.R
@@ -47,7 +47,7 @@ dust.total<-sum(values(dust.sum.hourly), na.rm=TRUE) #total mg for band
 dust.total<-dust.total/1000/1000 # mg to kg (total within burn perimeter)
 
 
-#---- iterate over fires --------------------------------
+#---- iterate over fires (30 days)--------------------------------
 setwd('/mnt/output/postfire_emissions/output/30day_output2save/dust')
 fires<-system('ls', intern=TRUE)
 
@@ -77,6 +77,63 @@ d$pm10_kg<-as.numeric(d$pm10_kg)
 d$pm10_kgperm2<-as.numeric(d$pm10_kgperm2)
 d$area_m2<-as.numeric(d$area_m2)
 
+
+#---- iterate over fires (annual)--------------------------------
+setwd('/mnt/output/postfire_emissions/output/annual_output2save/')
+fires<-system('ls batch_1/dust', intern=TRUE)
+
+dust.master <- data.frame(rbind(rep(NA,5)))
+names(dust.master)<-c('name', 'area_m2', 'start_month', 'pm10_kg', 'pm10_kgperm2')
+
+f1<-'batch_1/dust/'
+f2<-'batch_2/dust/'
+f3<-'batch_3/dust/'
+f4<-'batch_4/dust/'
+f5<-'batch_5/dust/'
+f6<-'batch_6/dust/'
+
+for(i in 1:1){
+    firename<-gsub("_dust.tif","", fires[i], fixed=TRUE)
+    firename<-checkSpaces(firename)
+    fire <- readOGR(dsn=dsn, layer=firename)
+    l<-c()
+    for(j in 1:6){
+        if(file.exists(paste0(get(paste0("f",j)),fires[i]))){
+            assign( paste0("dust",j), (brick(paste0(get(paste0("f",j)),fires[i]))) )
+            
+            #add up all values per pixel
+            assign( paste0("dust.sum", j), sum(get(paste0("dust", j))) )
+            #make a vector of dust.sumi filenames for access below
+            l<-c(l, paste0("dust.sum", j))     
+        }
+    }
+
+    if(length(l) == 4){    
+        dust.sum<-sum(get(l[1]), get(l[2]), get(l[3]), get(l[4]))
+    }
+    else if(length(l) == 5){
+        dust.sum<-sum(get(l[1]), get(l[2]), get(l[3]), get(l[4]), get(l[5]))
+    }
+    else if(length(l) == 6){
+        dust.sum<-sum(get(l[1]), get(l[2]), get(l[3]), get(l[4]), get(l[5]), get(l[6]))
+    }
+    rm(l)
+
+    dust.sum.hourly<-dust.sum*res(dust.sum)[1]^2*60*60 #mg/m2/s to total (for hour) mg per pixel
+    dust.total<-sum(values(dust.sum.hourly), na.rm=TRUE) #total mg for band
+    dust.total<-dust.total/1000/1000 # mg to kg (total within burn perimeter)
+    dust.kgperm2<-dust.total/fire$Area # kg/m2 (averaged over burned area)
+
+    dust.entry<-as.data.frame(cbind(as.character(fire$Fire_Name), fire$Area, fire$StartMonth, dust.total, dust.kgperm2))    
+    names(dust.entry)<-c('name', 'area_m2', 'start_month', 'pm10_kg', 'pm10_kgperm2')
+    
+    dust.master<-rbind(dust.master, dust.entry)
+}
+
+d<-na.omit(dust.master)
+d$pm10_kg<-as.numeric(d$pm10_kg)
+d$pm10_kgperm2<-as.numeric(d$pm10_kgperm2)
+d$area_m2<-as.numeric(d$area_m2)
 
 
 #--------------------------------------------------------
