b83436495235c93c89b06f2ba35ee7a26b408470
nwagenbrenner <nwagenbrenner@gmail.com>
2015-04-08 11:40:42 -0600

    calculate grid stats

--- src/windStats.R
+++ src/windStats.R
@@ -2,12 +2,27 @@ setwd('/home/natalie/postfire_emissions/windspeed_rdata')
 load('/mnt/output/postfire_emissions/output/annual_output2save/dustHOMERadded.Rdata')
 
 locations<-system('ls', intern=TRUE)
-percentGreaterThan10<-c()
+percent<-c()
 
 for(i in 1:length(locations)){
     load(locations[i])
     w<-as.data.frame(wind)
     s<-subset(w,subset=(max>10.0))
     p<-length(s$max)/length(w$max) * 100
-    percentGreaterThan10<-c(percentGreaterThan10, p)
+    percent<-c(percent, p)
 }
+
+#-----------------------------
+#  compute resolution stats
+#-----------------------------
+setwd('/mnt/output/postfire_emissions/output/annual_output2save/batch_1/dust')
+#get list of fire names
+fires<-system('ls', intern=TRUE)
+
+res<-c()
+for(i in 1:length(fires)){ 
+    assign( "b", (brick(fires[i])) )
+    print(paste0("reading ", fires[i]))
+    res<-c(res,res(b)[1])
+}
+
